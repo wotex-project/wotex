0de28c326a13682313d977e556b3da79e715738d
Tobias Bohwalli <hi@futhr.io>
2026-09-09 22:39:03 +0200

    chore: configure release tooling

--- packages/wotex-bacnet/mix.exs
+++ packages/wotex-bacnet/mix.exs
@@ -1,7 +1,7 @@
 defmodule WotexBACnet.MixProject do
   use Mix.Project
 
-  @version "0.1.0-dev"
+  @version "0.1.0"
   @source_url "https://github.com/wotex-project/wotex-bacnet"
 
   def project do
@@ -47,6 +47,7 @@ defmodule WotexBACnet.MixProject do
       {:telemetry, "~> 1.3"},
       {:stream_data, "~> 1.2", only: :test},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:git_ops, "~> 2.10", only: :dev, runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:doctor, "~> 0.22", only: [:dev, :test], runtime: false},
       {:ex_check, "~> 0.16", only: [:dev, :test], runtime: false},
